50eaef8cb83817bb365d2d3b63cb49b647d270c9
Nicholas Wheeler <nwheeler@devis.com>
2015-10-30 15:07:41 -0400

    Add support for managing cron in Debian.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,22 @@
 class awstats(
   $config_dir_purge = false,
   $enable_plugins   = [],
+  $crontab_manage   = false,
+  $crontab_minute   = '*/10',
+  $crontab_hour     = absent,
+  $crontab_month    = absent,
+  $crontab_monthday = absent,
+  $crontab_weekday  = absent,
+  $crontab_buildstatic = false,
+  $crontab_buildstatic_minute = '10',
+  $crontab_buildstatic_hour = '03',
+  $crontab_buildstatic_month = absent,
+  $crontab_buildstatic_monthday = absent,
+  $crontab_buildstatic_weekday = absent,
 ) inherits ::awstats::params {
   validate_bool($config_dir_purge)
+  validate_bool($crontab_manage)
+  validate_bool($crontab_buildstatic)
   validate_array($enable_plugins)
 
   package{ $::awstats::params::package_name: } ->
@@ -24,5 +38,41 @@ class awstats(
     anchor { 'awstats::begin': } ->
       Class[$load] ->
         anchor { 'awstats::end': }
+  }
+
+  if $::awstats::params::crontab_path {
+    file { $::awstats::params::crontab_path:
+      ensure =>  'file',
+      owner  =>  'root',
+      group  =>  'root',
+      mode   =>  '0755',
+      purge  =>  $crontab_manage,
+    }
+    if $crontab_manage {
+      cron { 'awstats':
+        ensure   =>  present,
+        command  =>  $::awstats::params::awstats_update_command,
+        user     =>  $::awstats::params::awstats_user,
+        minute   =>  $crontab_minute,
+        hour     =>  $crontab_hour,
+        month    =>  $crontab_month,
+        monthday =>  $crontab_monthday,
+        weekday  =>  $crontab_weekday,
+      }
+      cron { 'awstats_buildstatic':
+        ensure   =>  $crontab_buildstatic,
+        command  =>  $::awstats::params::awstats_buildstatic_command,
+        user     =>  $::awstats::params::awstats_user,
+        minute   =>  $crontab_buildstatic_minute,
+        hour     =>  $crontab_buildstatic_hour,
+        month    =>  $crontab_buildstatic_month,
+        monthday =>  $crontab_buildstatic_monthday,
+        weekday  =>  $crontab_buildstatic_weekday,
+      }
+    }
+  } else {
+    if $crontab_manage or $crontab_buildstatic {
+      fail("Managing crontab is not supported on ${::operatingsystem} / ${::operatingsystemmajrelease}")
+    }
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -7,9 +7,12 @@ class awstats::params {
     'RedHat': {
       case $::operatingsystemmajrelease {
         '6', '7': {
-          $package_name     = 'awstats'
-          $config_dir_path  = '/etc/awstats'
-          $default_template = "${module_name}/awstats.conf.erb"
+          $package_name                 = 'awstats'
+          $config_dir_path              = '/etc/awstats'
+          $default_template             = "${module_name}/awstats.conf.erb"
+          $crontab_path                 = undef
+          $awstats_update_command       = undef
+          $awstats_buildstatic_command  = undef
         }
         default: {
           fail("Module ${module_name} is not supported on operatingsystemmajrelease ${::operatingsystemmajrelease}") # lint:ignore:80chars
@@ -19,9 +22,12 @@ class awstats::params {
     'Debian': {
       case $::operatingsystemmajrelease {
         '8': {
-          $package_name     = 'awstats'
-          $config_dir_path  = '/etc/awstats'
-          $default_template = "${module_name}/awstats.conf.erb"
+          $package_name                 = 'awstats'
+          $config_dir_path              = '/etc/awstats'
+          $default_template             = "${module_name}/awstats.conf.erb"
+          $crontab_path                 = '/etc/cron.d/awstats'
+          $awstats_update_command       = '[ -x /usr/share/awstats/tools/update.sh ] && /usr/share/awstats/tools/update.sh'
+          $awstats_buildstatic_command  = '[ -x /usr/share/awstats/tools/buildstatic.sh ] && /usr/share/awstats/tools/buildstatic.sh'
         }
         default: {
           fail("Module ${module_name} is not supported on operatingsystemmajrelease ${::operatingsystemmajrelease}") # lint:ignore:80chars
